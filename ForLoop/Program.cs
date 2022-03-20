12d6438784187e3fd8f9f2d5823ccff040557419
uczburak <uczbrk@gmail.com>
2022-03-20 13:49:27 +0300

    Week1,Day2,Lesson3: Added arrays,collections, while and for loops.

---
+++ ForLoop/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] courses = { "C++", "C#", "Java", "Python", "JavaScript", "Sql" };
+
+
+            for (int i = 0; i < courses.Length; i++)
+            {
+                Console.WriteLine(courses[i]);
+            }
+
+            int[] ages = { 42, 29, 26, 29, 29, 23 };
+            int totalAge = 0;
+            for (int i = 0; i < ages.Length; i++)
+            {
+                totalAge += ages[i];
+            }
+
+            int average = totalAge / ages.Length;
+
+            Console.WriteLine($"Yaş ortalaması:{average}");
+
+            int[] numbers = { 36, 12, 26, 9, -4, 22, 8 };
+
+            int minNumber = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (minNumber > numbers[i])
+                {
+                    minNumber = numbers[i];
+                }
+            }
+
+            Console.WriteLine($"En küçük sayı:{minNumber}");
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Sayı tahmin oyunu
+               * Program rastgele bir sayı üretir.
+               * Kullanıcıdan tahmin istenir.
+               * Girilen tahmine göre aşağı ya da yukarı biçiminde yönlendirilir.
+               * Sayı bilinene dek tahmin istenir.
+             */
+            bool isFinished = false;
+            Random randomNumberGenerator = new Random();
+            
+            int randomNumber = randomNumberGenerator.Next(0, 100);
+            while (!isFinished)
+            {
+                // Kullanıcıdan her adımda bir tahmin iste
+                Console.WriteLine("Tahmininizi girin:");
+
+                int guess = Convert.ToInt32(Console.ReadLine());
+                // Aşağı ya da yukarı biçimde yönlendirme
+
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("Yukarı!");
+                }
+                else if (guess > randomNumber)
+                {
+                    Console.WriteLine("Aşağı!");
+                }
+                else
+                {
+                    Console.WriteLine("Bildiniz!");
+                    isFinished = true;
+                }
+            }
+        }
+    }
+}
